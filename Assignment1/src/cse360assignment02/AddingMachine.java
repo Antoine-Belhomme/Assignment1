93af4a271df1fcd98b57c9490f9e5c9afb75d4f6
Antoine <Antoine@DESKTOP-VN43889>
2020-10-05 17:43:25 -0700

    Initial commit project with couple of comments

---
+++ Assignment1/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,26 @@
+package cse360assignment02;
+
+public class AddingMachine {
+  private int total;    //An integer for the total result
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  
+  public int getTotal () { //Will return total 
+    return 0;
+  }
+  
+  public void add (int value) {  //Add a value to the total
+  }
+
+  public void subtract (int value) {  //Subtract a value to the total
+  }
+
+  public String toString () {  //Put all the operations into a string
+    return "";
+  }
+
+  public void clear() {
+  }
+}
